6c714e3bb69eab7c548bb8f0ac745e70b6ce228d
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-05 17:51:05 +0530

    fix :  format document and  fix some spelling mistake and spaces

--- models/bank_account.py
+++ models/bank_account.py
@@ -1,23 +1,21 @@
 class BankAccount:
-    def _init_(self, account_number, customer_name, balance=0.0):
+    def __init__(self, account_number, customer_name, balance=0.0):
         self.account_number = account_number
         self.customer_name = customer_name
         self.balance = balance
 
-    def deposit(self, ammount):
-        self.ammount += ammount
+    def deposit(self, amount):
+        self.balance += amount
 
-    def withdraw(self,amount):
-           if amount <= self.balance:
-             self.balance -= amount
-             return True
-
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            return True
         return False
 
     def display_account(self):
-        return(
+        return (
             f"Account Number: {self.account_number}\n"
             f"Customer Name: {self.customer_name}\n"
             f"balance: {self.balance:.2f}"
-            
         )
