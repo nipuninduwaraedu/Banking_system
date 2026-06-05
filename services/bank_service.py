e249484a6655870ab8995ea18dd310fec23c6e1e
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-06-05 18:58:49 +0530

    feat: create bankservice class

---
+++ services/bank_service.py
@@ -0,0 +1,19 @@
+from models.bank_account import BankAccount
+
+
+class bankservices:
+    def __init__(self):
+        self.account = []
+
+    def create_account(self, account_number, customer_name, balance):
+
+        account = BankAccount(
+            account_number,
+            customer_name,
+            balance
+        )
+
+
+self.account.append(account)
+
+return account
